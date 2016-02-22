69419b9ca2f229f6b223f1490288172c28786056
Ian Tuomi <ian.tuomi@gofore.com>
2016-02-22 16:29:36 +0200

    Add arrows to legend and title text

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-lineChart("ranking.csv", ["#0072C5", "#FFA824", '#E21831', '#3E0605'], "#line-chart");
+/* Parameters */
+var x_column = "Year";
+var dataset_names = ["Taiwan", "Shanghai", "QS", "Times"];
+var color_palette = ["#0072C5", "#FFA824", '#E21831', '#3E0605'];
+var dataset_file = "ranking.csv";
+var selector_parameter = "#line-chart";
+var lang = 'fi';
+var title_text =
+  { 'fi': 'Yliopistorankingit'
+  , 'en': 'University Rankings'
+  , 'sv': 'Universitetsrankningar'
+  }
+
+/* * */
+
+lineChart(dataset_file, color_palette, selector_parameter);
 
 function lineChart(filePath, colors, selector) {
-  var x_column = "Year";
   var constructedData;
 
   d3.dsv(";", "text/plain")(filePath, function(error, data) {
@@ -34,14 +48,14 @@ function lineChart(filePath, colors, selector) {
             };
           }
         })
-      }
+      };
     });
 
     drawSeries(constructedData);
   });
 
   var legend_y_scale = d3.scale.ordinal()
-    .domain(["Taiwan", "Shanghai", "QS", "Times"])
+    .domain(dataset_names)
     .range([0, 1, 2, 3]);
 
   function drawSeries() {
@@ -56,12 +70,12 @@ function lineChart(filePath, colors, selector) {
     var margin = {
       top: 20,
       right: 20,
-      bottom: 30,
+      bottom: 70,
       left: 50
     };
 
     var width = boundingWidth - margin.left - margin.right;
-    var height = (boundingWidth / 1.5) - margin.top - margin.bottom;
+    var height = (boundingWidth / 1.4) - margin.top - margin.bottom;
 
     var svg = d3.select(selector)
       .append("div")
@@ -145,6 +159,17 @@ function lineChart(filePath, colors, selector) {
       .data(constructedData)
       .enter();
 
+    var title_height = height/24;
+    svg.append("text")
+      .attr("x", width/2)
+      .attr("y", height + margin.bottom/1.25)
+      .style("text-anchor", "middle")
+      .style("font-family", "Open Sans")
+      .style("font-weight", "600")
+      .style("fill", "#222")
+      .style("font-style", "Italic")
+      .text(title_text[lang]);
+
     svg.append("g")
       .style("font-family", "Open Sans")
       .attr("class", "x axis")
@@ -169,7 +194,6 @@ function lineChart(filePath, colors, selector) {
         return color(d.name);
       })
       .attr('stroke-width', width/175)
-      .data(constructedData)
       .transition()
       .duration(transitionTime)
       .attrTween('d', function(d) {
@@ -224,33 +248,82 @@ function lineChart(filePath, colors, selector) {
     var tooltip_width = width/4;
     var ttip_top = height - tooltip_height;
 
+
     ttipScale = d3.scale.linear()
       .domain([0, width])
       .range([0, width - tooltip_width]);
 
-    var tooltip = svg.append("rect")
-      .attr("width", tooltip_width)
-      .attr("height", tooltip_height - tooltip_text_height/2)
-      .attr("y", ttip_top)
-      .classed("tooltip", true);
+    var tooltip_g = svg.append("g")
+      .classed("tooltip", true)
 
-    var legend = chart.append("g")
+    var true_tooltip_height = tooltip_height - tooltip_text_height/2;
+    var tooltip = tooltip_g.append("rect")
+      .attr("width", tooltip_width)
+      .attr("height", true_tooltip_height)
+      .attr("y", ttip_top);
+
+    var wedge_height = true_tooltip_height * 0.5;
+    var wedge_margin_y = (true_tooltip_height - wedge_height) / 2;
+
+    var wedge_width = true_tooltip_height * 0.1;
+    var wedge_margin_x = true_tooltip_height * 0.1;
+
+    var left = tooltip_g.append("g")
+      .attr("class", "arrow arrow--left");
+
+    left.append("line")
+      .attr("transform", "translate(0," + ttip_top + ")")
+      .attr("x1", -wedge_margin_x)
+      .attr("y1", wedge_margin_y)
+      .attr("x2", -(wedge_width + wedge_margin_x))
+      .attr("y2", wedge_margin_y + (wedge_height/2));
+
+    left.append("line")
+      .attr("transform", "translate(0," + ttip_top  + ")")
+      .attr("x1", -(wedge_width + wedge_margin_x))
+      .attr("y1", wedge_margin_y + (wedge_height/2))
+      .attr("x2", -wedge_margin_x)
+      .attr("y2", wedge_margin_y + wedge_height);
+
+    var right = tooltip_g.append("g")
+      .attr("class", "arrow arrow--right");
+
+    right.append("line")
+      .attr("transform", "translate(0," + ttip_top + ")")
+      .attr("x1", tooltip_width + wedge_margin_x)
+      .attr("y1", wedge_margin_y)
+      .attr("x2", tooltip_width + (wedge_width + wedge_margin_x))
+      .attr("y2", wedge_margin_y + (wedge_height/2));
+
+    right.append("line")
+      .attr("transform", "translate(0," + ttip_top  + ")")
+      .attr("x1", tooltip_width + (wedge_width + wedge_margin_x))
+      .attr("y1", wedge_margin_y + (wedge_height/2))
+      .attr("x2", tooltip_width + wedge_margin_x)
+      .attr("y2", wedge_margin_y + wedge_height);
+
+    function build_legend_data(d) {
+      return d.map(function(e) {
+        var y_values = {};
+        e.values.map(function(f) {
+          y_values[f.x] = f.y;
+        });
+        return {
+          name: e.name,
+          values: y_values
+        };
+      });
+    }
+
+    var legend = tooltip_g.append("g").selectAll("g")
+      .data(build_legend_data(constructedData))
+      .enter()
+      .append("g")
       .attr("class", "legend");
 
     var legend_height = 20;
     var legend_width = 20;
     var legend_padding = 10;
-
-    legend.datum(function(d) {
-      var y_values = {};
-      d.values.map(function(d) {
-        y_values[d.x] = d.y;
-      });
-      return {
-        name: d.name,
-        values: y_values
-      };
-    });
 
     var legend_y = function(d) {
       return ttip_top + tooltip_text_size + legend_y_scale(d.name) * tooltip_text_height;
@@ -259,6 +332,7 @@ function lineChart(filePath, colors, selector) {
     legend.append("text")
       .classed("legend-name", true)
       .text(function(d) {
+        console.log(d)
         return d.name;
       })
       .attr("text-anchor", "left")
@@ -266,6 +340,7 @@ function lineChart(filePath, colors, selector) {
       .style("font-family", "Open Sans")
       .style("font-weight", "Bold")
       .style("font-size", tooltip_text_size)
+      .attr("x", tooltip_text_height + tooltip_text_size )
       .attr("y", legend_y)
       .style("fill", function(d) {
         return d3.hcl(color(d.name)).darker(1);
@@ -282,6 +357,7 @@ function lineChart(filePath, colors, selector) {
       .style("font-family", "Open Sans")
       .style("font-weight", "Bold")
       .style("font-size", tooltip_text_size)
+      .attr("x", tooltip_width - tooltip_text_height/2)
       .attr("y", legend_y)
       .style("fill", function(d) {
         return d3.hcl(color(d.name)).darker(1);
@@ -295,6 +371,7 @@ function lineChart(filePath, colors, selector) {
       .style("fill", function(d) {
         return color(d.name);
       })
+      .attr("x", tooltip_text_height/2 )
       .attr("y", function(d) { return legend_y(d) - tooltip_text_size/1.8; })
       .attr("width", tooltip_text_size)
       .attr("height", tooltip_text_size);
@@ -319,6 +396,7 @@ function lineChart(filePath, colors, selector) {
       }
     }
 
+    var mouse_min = 2003;
     var mouse_max = 2015;
     var mouse_old = mouse_max;
 
@@ -344,33 +422,21 @@ function lineChart(filePath, colors, selector) {
         return defined(d, mouse_i) ? 1 : 0;
       })
 
-      legend.selectAll("rect")
-        .attr("x", tooltip_text_height/2 + ttipScale(x_scale(mouse_old)))
+      tooltip_g
+        .attr("transform", "translate(" + ttipScale(x_scale(mouse_old)) + ",0)")
         .transition(1000)
-        .attr("x", tooltip_text_height/2 + ttipScale(x_scale(mouse_i)));
-
-      legend.selectAll("text.legend-name")
-        .attr("x", tooltip_text_height + tooltip_text_size + ttipScale(x_scale(mouse_old)))
-        .transition(1000)
-        .attr("x", tooltip_text_height + tooltip_text_size + ttipScale(x_scale(mouse_i)));
+        .attr("transform", "translate(" + ttipScale(x_scale(mouse_i)) + ",0)");
 
       legend.selectAll("text.legend-number")
         .text(function(d) {
           return defined(d, mouse_i) ? d.values[mouse_i] : '';
-        })
-        .attr("x", tooltip_width - tooltip_text_height/2 + ttipScale(x_scale(mouse_old)))
-        .transition(1000)
-        .attr("x", tooltip_width - tooltip_text_height/2  + ttipScale(x_scale(mouse_i)));
+        });
 
       focusline.attr("x1", x_scale(mouse_old))
         .attr("x2", x_scale(mouse_old))
         .transition(1000)
         .attr("x1", x_scale(mouse_i))
         .attr("x2", x_scale(mouse_i));
-
-      tooltip.attr("x", ttipScale(x_scale(mouse_old)))
-        .transition(1000)
-        .attr("x", ttipScale(x_scale(mouse_i)));
 
       d3.selectAll(".year")
         .style("font-weight", "normal");
@@ -379,6 +445,19 @@ function lineChart(filePath, colors, selector) {
         d3.selectAll('.year_' + mouse_i)
           .style("font-weight", "Bold");
       }
+
+      d3.selectAll(".arrow")
+        .style("display", "initial");
+
+      if (mouse_i === mouse_max) {
+        d3.selectAll(".arrow--right")
+          .style("display", "none");
+      }
+      else if (mouse_i === mouse_min) {
+        d3.selectAll(".arrow--left")
+          .style("display", "none");
+      }
+
 
       mouse_old = mouse_i;
     }
